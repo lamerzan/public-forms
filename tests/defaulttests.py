41d57f4dfa1ea0d6e6eb5c7c4259973b58de085d
Denis Mishchishin <dennz78@gmail.com>
2011-12-09 15:09:20 +0200

    gridsystem skeleton init

--- tests/defaulttests.py
+++ tests/defaulttests.py
@@ -3,9 +3,9 @@ from utility import VirtualProjectTestCase
 
 class TestImportable(VirtualProjectTestCase):
     def test_package_importable(self):
-        out, err = self.command('''python -c "from {package_namespace} import {egg_name}; print {egg_name}; print 'marKer'"''', output=True)
+        out, err = self.command('''python -c "from feincms.page.extensions import gridsystem; print gridsystem; print 'marKer'"''', output=True)
         out = '%s'%out
-        self.assert_('{package_namespace}' in out)
-        self.assert_('{egg_name}' in out)
+        self.assert_('feincms.page.extensions' in out)
+        self.assert_('gridsystem' in out)
         self.assert_('module' in out)
         self.assert_('marKer' in out)
